217ed543e4c9ef22a183a37deb58603bae7855a4
Heine Günther <heine_gunther@hotmail.com>
2024-04-29 11:31:26 +0200

    Added dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,37 @@
 import { SearchInput } from "@/components/SearchInput";
+import ThemeSwitch from "@/components/ThemeSwitch";
 import { Button } from "@/components/ui/button";
 
+function TopNav() {
+  return (
+    <nav className="h-20 flex justify-between items-center border-b-2 px-4">
+      <div className="flex items-center space-x-4">
+        <span className="font-bold text-lg">Logo</span>
+        <SearchInput placeholder="Search..." />
+      </div>
+      <div className="flex items-center space-x-4">
+        <ThemeSwitch />
+      </div>
+    </nav>
+  );
+}
 export default function Home() {
   return (
-    <main className="h-screen flex flex-col gap-2 justify-center items-center">
-      <form role="search">
-        <SearchInput placeholder="Something...." />
-      </form>
-      <Button>Default</Button>
-      <Button size="sm">small</Button>
-      <Button size="lg">LARGE</Button>
-      <Button size="icon">i</Button>
-      <Button variant="outline">outline</Button>
-      <Button variant="destructive">cancel</Button>
-      <Button variant="ghost">ghost</Button>
-      <Button variant="secondary">2nd</Button>
-      <Button variant="ghost" size="icon" className="rounded-full">
-        🖕
-      </Button>
-    </main>
+    <>
+      <TopNav />
+      <main className="h-screen flex flex-col gap-2 justify-center items-center">
+        <Button>Default</Button>
+        <Button size="sm">small</Button>
+        <Button size="lg">LARGE</Button>
+        <Button size="icon">i</Button>
+        <Button variant="outline">outline</Button>
+        <Button variant="destructive">cancel</Button>
+        <Button variant="ghost">ghost</Button>
+        <Button variant="secondary">2nd</Button>
+        <Button variant="ghost" size="icon" className="rounded-full">
+          🖕
+        </Button>
+      </main>
+    </>
   );
 }
